778ebe8c2223b6ab9cd0ad67a658c5de0b4b2435
Devil4urs <140965514+Devil4urs@users.noreply.github.com>
2024-09-27 11:12:25 +0530

    files added

---
+++ lib/constants/colors.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class mainColor {
+  static Color primaryColor = Color.fromARGB(255, 0,71, 113);
+  static Color secondaryColor = Color.fromARGB(255, 200, 134, 145);
+  static Color accentColor = Color.fromARGB(255, 33, 41, 48);
+}
